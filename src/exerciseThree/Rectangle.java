b7e217f39c12e1e7c5114e83f125d7fdd52e8356
Romina <rominacaballero@gmail.com>
2024-10-10 03:34:56 -0300

    Added the exercise three completed

---
+++ src/exerciseThree/Rectangle.java
@@ -0,0 +1,35 @@
+package exerciseThree;
+
+public class Rectangle {
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+}
