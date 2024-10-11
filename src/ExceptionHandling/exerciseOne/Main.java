58e6318091b280d794038ab7d5d470322f73f849
Romina <rominacaballero@gmail.com>
2024-10-11 03:17:28 -0300

    Added exerciseOne for the new exceptionHandling folder

---
+++ src/ExceptionHandling/exerciseOne/Main.java
@@ -0,0 +1,19 @@
+package ExceptionHandling.exerciseOne;
+//Write a Java program that throws an exception and catch it using a try-catch block.
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int result = divideNumbers(5, 0);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static int divideNumbers(int dividend, int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException("Cannot divide the given number by zero!");
+        }
+        return dividend / divisor;
+    }
+}
