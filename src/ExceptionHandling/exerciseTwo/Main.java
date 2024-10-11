c4811fe3bc741910c589a513925563b922d2eee4
Romina <rominacaballero@gmail.com>
2024-10-11 04:04:31 -0300

    Added exerciseTwo to the ExceptionHandling folder

---
+++ src/ExceptionHandling/exerciseTwo/Main.java
@@ -0,0 +1,27 @@
+package ExceptionHandling.exerciseTwo;
+/* Write a Java program to create a method that takes an integer
+ as a parameter and throws an exception if the number is odd.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int number = 2;
+        takeInteger(number);
+        number = 3;
+        takeInteger(number);
+
+    }
+    public static void takeInteger(int number) {
+        try {
+            checkEvenNumber(number);
+            System.out.println(number + " is even");
+        } catch (IllegalArgumentException e) {
+            System.out.println(number + " is odd");
+        }
+    }
+
+    public static void checkEvenNumber(int number) {
+        if (number % 2 != 0) {
+            throw new IllegalArgumentException(number + " is odd.");
+        }
+    }
+}
