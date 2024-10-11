e5c2305e60488f3022f373991efeb6f6d82dc781
Romina <rominacaballero@gmail.com>
2024-10-11 05:25:18 -0300

    Added exerciseFive for ExceptionHandling folder and a txt file test1.txt

---
+++ src/ExceptionHandling/exerciseFive.java
@@ -0,0 +1,34 @@
+package ExceptionHandling;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//Write a Java program that reads a file and throws an exception if the file is empty.
+public class exerciseFive {
+    public static void main(String[] args) {
+        try {
+            readFile("test1.txt");
+            System.out.println("File is not empty");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (EmptyFileException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static void readFile(String fileName) throws FileNotFoundException, EmptyFileException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+
+        if (!scanner.hasNextLine()) {
+            throw new EmptyFileException("Empty file");
+        }
+        scanner.close();
+    }
+}
+
+class EmptyFileException extends Exception {
+    public EmptyFileException(String message) {
+        super(message);
+    }
+}
